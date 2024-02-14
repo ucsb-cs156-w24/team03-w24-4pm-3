d1316dc8ddff7d1a9747d4b546595c86333f6d51
Sid Rambhatla <srambhatla@ucsb.edu>
2024-02-14 01:46:07 -0800

    a

--- frontend/src/tests/components/HelpRequest/HelpRequestTable.test.js
+++ frontend/src/tests/components/HelpRequest/HelpRequestTable.test.js
@@ -16,7 +16,7 @@ describe("UserTable tests", () => {
   const queryClient = new QueryClient();
 
   const expectedHeaders = ["id", "RequesterEmail", "TeamId", "TableOrBreakoutRoom", "RequestTime", "Explanation", "Solved"];
-  const expectedFields = ["id", "requesterEmail", "teamId", "tableOrBreakoutRoom", "requestTime", "explanation", "Solved"];
+  const expectedFields = ["id", "requesterEmail", "teamId", "tableOrBreakoutRoom", "requestTime", "explanation", "solved"];
   const testId = "HelpRequestTable";
 
   test("renders empty table correctly", () => {
