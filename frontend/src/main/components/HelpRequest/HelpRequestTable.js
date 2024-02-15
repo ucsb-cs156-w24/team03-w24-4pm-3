d78d32702f324896ee9777d1de85242a0274e179
Sid Rambhatla <srambhatla@ucsb.edu>
2024-02-14 23:21:00 -0800

    a

--- frontend/src/main/components/HelpRequest/HelpRequestTable.js
+++ frontend/src/main/components/HelpRequest/HelpRequestTable.js
@@ -14,7 +14,7 @@ export default function HelpRequestTable({
     const navigate = useNavigate();
 
     const editCallback = (cell) => {
-        navigate(`/helprequests/edit/${cell.row.values.id}`)
+        navigate(`/helprequest/edit/${cell.row.values.id}`)
     }
 
     // Stryker disable all : hard to test for query caching
