a117ba74b0d354d50ee3b92ce48967d7c560a777
Laya Pullela <lpullela@ucsb.edu>
2024-02-15 17:21:48 -0800

    LP fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationsFixtures.js
@@ -0,0 +1,35 @@
+const ucsbDatesFixtures = {
+    onebOrganization: {
+        "id": 1,
+        "orgCode": "ZPR",
+        "orgTranslationShort": "ZPR",
+        "orgTranslation": "Zeta Phi Rho",
+        "inactive": "false"
+    },
+    threebOrganizations: [
+        {
+            "id": 1,
+            "orgCode": "ZPR",
+            "orgTranslationShort": "ZPR",
+            "orgTranslation": "Zeta Phi Rho",
+            "inactive": "false"
+        },
+        {
+            "id": 2,
+            "orgCode": "DSP",
+            "orgTranslationShort": "DSP",
+            "orgTranslation": "Delta Sigma Pi",
+            "inactive": "false"
+        },
+        {
+            "id": 3,
+            "orgCode": "ZBT",
+            "orgTranslationShort": "ZBT",
+            "orgTranslation": "Zeta Beta Tau",
+            "inactive": "false"
+        }
+    ]
+};
+
+
+export { ucsbOrganizationsFixtures };
