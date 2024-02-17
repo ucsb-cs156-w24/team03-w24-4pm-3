c967c9228d02b9633af57083b60a2982177272a2
Sid Rambhatla <srambhatla@ucsb.edu>
2024-02-16 22:45:29 -0800

    a

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -4,7 +4,7 @@ const helpRequestFixtures = {
             "id": 1,
             "requesterEmail": "srambhatla@ucsb.edu",
             "teamId": "01",
-            "tableOrBreakoutRoom": "01",
+            "tableOrBreakoutRoom": "Table 1",
             "requestTime": "2022-01-02T12:00:00",
             "explanation": "Dokku problems",
             "solved": true
@@ -14,7 +14,7 @@ const helpRequestFixtures = {
             "id": 2,
             "requesterEmail": "one@ucsb.edu",
             "teamId": "01",
-            "tableOrBreakoutRoom": "01",
+            "tableOrBreakoutRoom": "Table 1",
             "requestTime": "2022-01-02T12:00:00",
             "explanation": "Dokku issues",
             "solved": true
@@ -23,7 +23,7 @@ const helpRequestFixtures = {
             "id": 3,
             "requesterEmail": "two@ucsb.edu",
             "teamId": "02",
-            "tableOrBreakoutRoom": "02",
+            "tableOrBreakoutRoom": "Table 2",
             "requestTime": "2022-04-03T12:00:00",
             "explanation": "Swagger problems",
             "solved": false
@@ -32,7 +32,7 @@ const helpRequestFixtures = {
             "id": 4,
             "requesterEmail": "three@ucsb.edu",
             "teamId": "03",
-            "tableOrBreakoutRoom": "03",
+            "tableOrBreakoutRoom": "Table 3",
             "requestTime": "2022-07-04T12:00:00",
             "explanation": "Merge conflict",
             "solved": true
