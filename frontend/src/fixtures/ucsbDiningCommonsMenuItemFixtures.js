882fc30f8ed4c5e55d5dd680ef836ac099366624
Konark Vinod <konark@MacBook-Pro.local>
2024-02-20 13:14:04 -0800

    added menu items fixture

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,38 @@
+const restaurantFixtures = {
+    oneRestaurant:
+    [
+      {
+        "id": 1,
+        "diningCommonsCode": "Ortega",
+        "name": "Baked Pesto Pasta with Chicken",
+        "station": "Entrees"      
+      }
+    ],
+
+    threeRestaurants:
+    [
+        {
+            "id": 2,
+            "diningCommonsCode": "Portola",
+            "name": "Turkey Rice Soup",
+            "station": "Greens & Grains"    
+        },
+
+        {
+            "id": 3,
+            "diningCommonsCode": "De La Guerra",
+            "name": "Tri Tip Roast",
+            "station": "Blue Plate Special"  
+        },
+
+        {
+            "id": 4,
+            "diningCommonsCode": "Carrillo",
+            "name": "Egg Salad",
+            "station": "Deli"      
+        },
+        
+    ]
+};
+
+export { restaurantFixtures };
