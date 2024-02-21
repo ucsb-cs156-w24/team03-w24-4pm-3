2b3df97e4b691ee254c6bbe25fdce60e7c3edd8e
Sid Rambhatla <srambhatla@ucsb.edu>
2024-02-20 22:06:58 -0800

    a

--- src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
@@ -52,7 +52,7 @@ public class HelpRequestController extends ApiController {
             @Parameter(name="requesterEmail") @RequestParam String requesterEmail,
             @Parameter(name="teamId") @RequestParam String teamId,
             @Parameter(name="tableOrBreakoutRoom") @RequestParam String tableOrBreakoutRoom,
-            @Parameter(name="date") @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime requestTime,
+            @Parameter(name="requestTime") @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime requestTime,
             @Parameter(name="explanation") @RequestParam String explanation,
             @Parameter(name="solved") @RequestParam boolean solved)
             throws JsonProcessingException {
